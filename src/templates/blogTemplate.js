ae8582ce192cdf0dc978a5146b31813a65d3bfd6
bmumz <brandimummery@gmail.com>
2020-06-04 19:46:45 -0400

    update

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { graphql, Link } from "gatsby"
+import { graphql } from "gatsby"
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 import Layout from "../components/layout"
 import templateStyles from "./template.module.scss"
