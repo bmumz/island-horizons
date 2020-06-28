71b380468e7c776871d7123f1ae3615bc4e73fc2
bmumz <brandimummery@gmail.com>
2020-06-27 23:23:42 -0400

    updated og:image

--- src/components/head.js
+++ src/components/head.js
@@ -40,7 +40,7 @@ const Head = ({ title, keywords, description }) => {
         name="url"
         content={data.site.siteMetadata.siteUrl}
       />
-      <meta property="og:image" content="https://i.ibb.co/Y0yM65D/og-img.jpg" />
+      <meta property="og:image" content="https://i.ibb.co/C017MWN/og-img.jpg" />
 
       <link rel="canonical" href="https://www.islandhorizons.com"></link>
     </Helmet>
