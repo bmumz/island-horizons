f622b239641866502a3d1ccd1ea3b113ce781ad3
bmumz <brandimummery@gmail.com>
2020-07-21 17:00:06 -0400

    added favicons, manifest.json file

--- gatsby-config.js
+++ gatsby-config.js
@@ -120,5 +120,20 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Island Horizons",
+        short_name: "Island Horizons",
+        start_url: "/",
+        background_color: "#7cbc9b",
+        theme_color: "#adacac",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "browser",
+        icon: "src/images/icon.png",
+        crossOrigin: `use-credentials`,
+      },
+    },
   ],
 }
